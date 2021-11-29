bb4ac8b139914f9b5432e576718162d62d44e108
sanjay-amoment4u <sanjay.2wardsuccess@gmail.com>
2021-11-29 13:40:05 +0530

    Code check-ins for other functionalities of Functional Interface examples.

--- src/com/wit/lambda/samples/FunctionalInterfaceEx.java
+++ src/com/wit/lambda/samples/FunctionalInterfaceEx.java
@@ -1,5 +1,12 @@
 package com.wit.lambda.samples;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
 /**
  * This class contains the info and examples on Functional Interface and it's usability.
  * 
@@ -20,8 +27,40 @@ package com.wit.lambda.samples;
 public class FunctionalInterfaceEx {
 
     public static void main(String[] args) {
+        //Lambda usage with a custom functional interface
         MyFunctionalInterfaceNoArg myFunctionalInterfaceNoArg = () -> System.out.println("Lambda with custom functional interface.");
         myFunctionalInterfaceNoArg.printMessage();
+        
+        //Lambda usage with an in-built Functional Interface - Predicate
+        String name = "Sanjay";
+        String startsWithChar = "S";
+        //Declaring the predicate type as string and use lambda expression
+        Predicate<String> predicate = s -> s.startsWith(startsWithChar);
+        System.out.println("The name " + name + " starts with " + startsWithChar + " : " + predicate.test(name));
+        
+        //Lambda usage with an in-built Functional Interface - Consumers
+        Map<String, Integer> persons = new HashMap<>();
+        persons.put("Sanjay", 35);
+        persons.put("Kumar", 24);
+        persons.put("Samarth", 6);
+        persons.forEach((names, ages) -> System.out.println(names + " is " + ages + " years old"));
+        
+        //Lambda usage with an in-built Functional Interface - Consumers
+        Consumer<String> consumer1 = FunctionalInterfaceEx::printMessage;  
+        consumer1.accept("Kumar");   //calling Consumer method
+        
+        //Lambda usage with an in-built Functional Interface - Operators
+        List<String> people = Arrays.asList("Sanjay", "Kumar", "Samarth");
+        people.replaceAll(String::toUpperCase);
+        people.forEach(n -> System.out.println(n));
+        
+        //Lambda usage with the Legacy functional interface
+        Thread thread = new Thread(() -> System.out.println("Hello from a different thread with the legacy Functional Interface!"));
+        thread.start();
+    }
+    
+    static void printMessage(String name){  
+        System.out.println("Hello "+name);  
     }
 }
 
